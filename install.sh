c4cbb59c08023b0caa48ecd51ee5c4ae49fe6baf
sobadon <sbdn@chasoba.net>
2021-05-23 20:39:47 +0900

    install.sh が含まれていないのを修正

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# install dein.vim
+mkdir -p ~/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh ./installer.sh ~/.cache/dein
+rm ./installer.sh
+
+rm -rf ~/.config
+rm -rf ~/.tmux.conf
+rm -rf ~/.bashrc
+rm -rf ~/.zshrc
+rm -rf ~/.vimrc
+rm -rf ~/.ssh/rc
+
+
+DOT_DIR="${HOME}/dotfiles"
+
+#  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+
+ln -snfv ${DOT_DIR}/.config ~/.config
+ln -snfv ${DOT_DIR}/.tmux.conf ~/.tmux.conf
+ln -snfv ${DOT_DIR}/.bashrc ~/.bashrc
+ln -snfv ${DOT_DIR}/.zshrc ~/.zshrc
+ln -snfv ${DOT_DIR}/.vimrc ~/.vimrc
+ln -snfv ${DOT_DIR}/.ssh/rc ~/.ssh/rc
+
