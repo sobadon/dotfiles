2a1344b40c3aacae9ec7034fdc9a746b4e1b821d
sobadon <sbdn@chasoba.net>
2022-07-17 01:49:53 +0900

    small ならば small から

--- install_small.sh
+++ install_small.sh
@@ -14,15 +14,15 @@ mv peco_linux_amd64/peco ~/.local/bin/
 rm -rf peco_linux_amd64.tar.gz peco_linux_amd64
 
 
-rm -rf ~/.config
-rm -rf ~/.tmux.conf
+# rm -rf ~/.config
+# rm -rf ~/.tmux.conf
 rm -rf ~/.bashrc
 rm -rf ~/.zshrc
 rm -rf ~/.ssh/rc
 
-DOT_DIR="${HOME}/dotfiles"
-ln -snfv ${DOT_DIR}/.config ~/.config
-ln -snfv ${DOT_DIR}/.tmux.conf ~/.tmux.conf
+DOT_DIR="${HOME}/dotfiles/small"
+# ln -snfv ${DOT_DIR}/.config ~/.config
+# ln -snfv ${DOT_DIR}/.tmux.conf ~/.tmux.conf
 ln -snfv ${DOT_DIR}/.bashrc ~/.bashrc
 ln -snfv ${DOT_DIR}/.zshrc ~/.zshrc
 ln -snfv ${DOT_DIR}/.ssh/rc ~/.ssh/rc
