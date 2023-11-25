6b27da6f7c1e3628eef784ed847fa568e6b69e20
sobadon <sbdn@chasoba.net>
2023-11-26 02:06:36 +0900

    Windows Update したら npiperelay.exe が起動しなくなったので WSL_INTEROP を追加
    2023/11 頃の Windows Update でおかしくなった
    普通に zsh シェルから socat 直で実行したら動いた

---
+++ .config/systemd/user/wsl2-ssh-agent-exec.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# systemd user service によって起動されると環境変数 WSL_INTEROP が存在しない。
+# - https://github.com/microsoft/WSL/issues/8846
+# - https://github.com/microsoft/WSL/issues/5065
+
+# 環境変数 WSL_INTEROP が存在しないと npiperelay.exe の実行に失敗し、
+# 結果として ssh-add -l で error fetching identities: communication with agent failed のエラーになる。
+
+# とりあえず末尾 怪しい
+export WSL_INTEROP=/run/WSL/`ls -tr /run/WSL | tail -n1`
+echo aa "$WSL_INTEROP"
+socat "UNIX-LISTEN:${HOME}/.ssh/agent.sock,mode=600,fork" "EXEC:${HOME}/.ssh/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent,nofork"
