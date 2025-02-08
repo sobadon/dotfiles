a36b9663247b9f835bdc8473a6affa04b1ad47ba
sobadon <sbdn@chasoba.net>
2025-02-08 14:48:04 +0900

    fix wezterm skk shift

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -5,7 +5,7 @@ config.font = wezterm.font 'UDEV Gothic 35NF'
 config.font_size = 14.0
 
 -- SKK
-config.macos_forward_to_ime_modifier_mask = "CTRL"
+config.macos_forward_to_ime_modifier_mask = 'SHIFT|CTRL'
 
 config.keys = {
   -- Command + W -> Command + Shift + W
