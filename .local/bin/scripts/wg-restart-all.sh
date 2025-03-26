f0b11b8ef79a5cfa9ab6dde300bb442386afc3af
sobadon <sbdn@chasoba.net>
2025-03-26 20:01:50 +0900

    wg-restart-all.sh

---
+++ .local/bin/scripts/wg-restart-all.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -u
+
+if [[ $(hostname) == "nade2" ]]; then
+    wg_ifs=("wg0" "wg1", "wg2", "wg3")
+    for wg_if in ${wg_ifs[@]}; do
+        sudo wg-quick down "${wg_if}" ; sudo wg-quick up "${wg_if}"
+    done
+fi
