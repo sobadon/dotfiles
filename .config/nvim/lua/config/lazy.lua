413f8df9d4cb3f1e56a03c89f4bd0eee0e548635
sobadon <sbdn@chasoba.net>
2025-04-05 00:06:43 +0900

    fix format

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -107,7 +107,7 @@ require("lazy").setup({
       --   -- Example mapping to toggle outline
       --   vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>",
       --     { desc = "Toggle Outline" })
-    
+
       --   require("outline").setup {
       --     -- Your setup opts here (leave empty to use defaults)
       --   }
