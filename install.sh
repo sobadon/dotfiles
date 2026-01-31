6f7bf699e0809859e878419e310aea766a435f24
sobadon <sbdn@chasoba.net>
2026-01-31 08:18:39 +0000

    だいたいべきとう

--- install.sh
+++ install.sh
@@ -1,27 +1,37 @@
 #!/bin/bash
 
-# install asdf
+set -eu
+set -o pipefail
+
 # https://asdf-vm.com/guide/getting-started.html#_2-download-asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
-
-rm -rf ~/.config
-rm -rf ~/.tmux.conf
-rm -rf ~/.bashrc
-rm -rf ~/.zshrc
-rm -rf ~/.ssh/rc
-rm -rf ~/.digrc
-
+if [ ! -d ~/.asdf ]; then
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.10.2
+fi
 
 DOT_DIR="${HOME}/dotfiles"
 
-mkdir -p ~/.local/bin/scripts
+mkdir -p ~/.config
+ln -snf ${DOT_DIR}/.config/aquaproj-aqua ~/.config/aquaproj-aqua
+ln -snf ${DOT_DIR}/.config/git ~/.config/git
+ln -snf ${DOT_DIR}/.config/nvim ~/.config/nvim
+ln -snf ${DOT_DIR}/.config/systemd ~/.config/systemd
+ln -snf ${DOT_DIR}/.config/wezterm ~/.config/wezterm
+ln -snf ${DOT_DIR}/.config/zsh ~/.config/zsh
 
-#  ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
+rm -rf ~/.tmux.conf
+ln -snf ${DOT_DIR}/.tmux.conf ~/.tmux.conf
 
-ln -snfv ${DOT_DIR}/.config ~/.config
-ln -snfv ${DOT_DIR}/.tmux.conf ~/.tmux.conf
-ln -snfv ${DOT_DIR}/.bashrc ~/.bashrc
-ln -snfv ${DOT_DIR}/.zshrc ~/.zshrc
-ln -snfv ${DOT_DIR}/.ssh/rc ~/.ssh/rc
-ln -snfv ${DOT_DIR}/.digrc ~/.digrc
-ln -snfv ${DOT_DIR}/.local/bin/scripts ~/.local/bin/
+rm -rf ~/.bashrc
+ln -snf ${DOT_DIR}/.bashrc ~/.bashrc
+
+rm -rf ~/.zshrc
+ln -snf ${DOT_DIR}/.zshrc ~/.zshrc
+
+mkdir -p ~/.ssh
+rm -rf ~/.ssh/rc
+ln -snf ${DOT_DIR}/.ssh/rc ~/.ssh/rc
+
+rm -rf ~/.digrc
+ln -snf ${DOT_DIR}/.digrc ~/.digrc
+
+ln -snf ${DOT_DIR}/.local/bin/scripts ~/.local/bin/
--- install_small.sh
+++ install_small.sh
@@ -3,9 +3,6 @@
 set -eu
 set -o pipefail
 
-# prepare custom binary
-mkdir -p ~/.local/bin/
-
 # install peco
 PECO_VERSION=v0.5.10
 wget https://github.com/peco/peco/releases/download/${PECO_VERSION}/peco_linux_amd64.tar.gz
@@ -13,24 +10,26 @@ tar xzvf peco_linux_amd64.tar.gz peco_linux_amd64/peco
 mv peco_linux_amd64/peco ~/.local/bin/
 rm -rf peco_linux_amd64.tar.gz peco_linux_amd64
 
-
-rm -rf ~/.config
-rm -rf ~/.tmux.conf
-rm -rf ~/.bashrc
-rm -rf ~/.zshrc
-rm -rf ~/.ssh/rc
-rm -rf ~/.digrc
-
 DOT_DIR="${HOME}/dotfiles/small"
-ln -snfv ${DOT_DIR}/.config ~/.config
-ln -snfv ${DOT_DIR}/.tmux.conf ~/.tmux.conf
-ln -snfv ${DOT_DIR}/.bashrc ~/.bashrc
-ln -snfv ${DOT_DIR}/.zshrc ~/.zshrc
-mkdir -p ~/.ssh
-ln -snfv ${DOT_DIR}/.ssh/rc ~/.ssh/rc
-ln -snfv ${DOT_DIR}/.digrc ~/.digrc
 
-# まぜこぜ
-mkdir -p ~/.local/bin/scripts
-DOT_DIR="${HOME}/dotfiles"
-ln -snfvd ${DOT_DIR}/.local/bin/scripts ~/.local/bin/
+mkdir -p ~/.config
+ln -snf ${DOT_DIR}/.config/git ~/.config/git
+ln -snf ${DOT_DIR}/.config/zsh ~/.config/zsh
+
+rm -rf ~/.tmux.conf
+ln -snf ${DOT_DIR}/.tmux.conf ~/.tmux.conf
+
+rm -rf ~/.bashrc
+ln -snf ${DOT_DIR}/.bashrc ~/.bashrc
+
+rm -rf ~/.zshrc
+ln -snf ${DOT_DIR}/.zshrc ~/.zshrc
+
+mkdir -p ~/.ssh
+rm -rf ~/.ssh/rc
+ln -snf ${DOT_DIR}/.ssh/rc ~/.ssh/rc
+
+rm -rf ~/.digrc
+ln -snf ${DOT_DIR}/.digrc ~/.digrc
+
+ln -snf ${DOT_DIR}/.local/bin/scripts ~/.local/bin/
