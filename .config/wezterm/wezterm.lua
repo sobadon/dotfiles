b977226f37bf4ff0249bedc7d86237cb99d59ce8
sobadon <sbdn@chasoba.net>
2026-02-10 23:41:23 +0900

    wezterm Windows C-c シグナル処理

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+local act = wezterm.action
 
 config.font = wezterm.font 'UDEV Gothic 35NF'
 config.font_size = 14.0
@@ -26,6 +27,21 @@ if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
   config.default_domain = 'WSL:Debian'
   config.font_size = 10.0
   config.window_decorations = "RESIZE"
+
+  table.insert(config.keys, {
+      key = 'c',
+      mods = 'CTRL',
+      action = wezterm.action_callback(function(window, pane)
+        local selection = window:get_selection_text_for_pane(pane)
+        if selection ~= "" then
+          -- 選択範囲があるため クリップボードにコピー
+          window:perform_action(act.CopyTo 'ClipboardAndPrimarySelection', pane)
+        else
+          -- 選択範囲がないため Ctrl+C のキー入力を送信（プロセス中断）
+          window:perform_action(act.SendKey { key = 'c', mods = 'CTRL' }, pane)
+        end
+      end),
+    })
 end
 
 return config
