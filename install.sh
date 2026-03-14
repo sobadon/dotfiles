9bdf3d34084a8aefdcf15d05ec818324d27f4370
sobadon <sbdn@chasoba.net>
2026-03-15 01:16:42 +0900

    mise install.sh?

--- install.sh
+++ install.sh
@@ -10,11 +10,20 @@ fi
 
 export PATH=${AQUA_ROOT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/aquaproj-aqua}/bin:$PATH
 if ! type aqua > /dev/null 2>&1; then
-curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v4.0.2/aqua-installer
-echo "98b883756cdd0a6807a8c7623404bfc3bc169275ad9064dc23a6e24ad398f43d  aqua-installer" | sha256sum -c -
-chmod +x aqua-installer
-./aqua-installer
-rm -rf ./aqua-installer
+  curl -sSfL -O https://raw.githubusercontent.com/aquaproj/aqua-installer/v4.0.2/aqua-installer
+  echo "98b883756cdd0a6807a8c7623404bfc3bc169275ad9064dc23a6e24ad398f43d  aqua-installer" | sha256sum -c -
+  chmod +x aqua-installer
+  ./aqua-installer
+  rm -rf ./aqua-installer
+fi
+
+if ! type mise > /dev/null 2>&1; then
+  curl -sSfL -O https://github.com/jdx/mise/releases/download/v2026.3.9/install.sh
+  echo "56f59cf2e41d0120cc2a36caf3e4d68c2a420c2a03402901490a24ab32a1a594  install.sh" | sha256sum -c -
+  chmod +x install.sh
+  export MISE_QUIET=1
+  ./install.sh
+  rm -rf ./install.sh
 fi
 
 DOT_DIR="${HOME}/dotfiles"
